76f12c506eff17ff26e11da7198db4274680875e
compitcomds <154963261+compitcomds@users.noreply.github.com>
2024-03-20 19:06:07 +0530

    Update bunny.py

--- Solar/bunny.py
+++ Solar/bunny.py
@@ -2,10 +2,8 @@ from BunnyCDN.CDN import CDN
 from BunnyCDN.Storage import Storage
 from config import Config
 
-storage_api_key=Config.STORAGE_API_KEY
-storage_api_key=Config.STORAGE_API_KEY
-storage_zone_name=Config.STORAGE_ZONE_NAME
+storage_api_key="7ac93fd8-09f9-4a9f-acc6c3515889-6e83-44d0"
 storage_zone_name="stocksales"
 
 obj_storage = Storage(storage_api_key,storage_zone_name)
-obj_cdn = CDN(Config.STORAGE_CDN)
+obj_cdn = CDN("cdfca33f-7b42-4cd9-970a-d9125ad09fde")
