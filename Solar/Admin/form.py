3ee3ce1472bd983c4c53dacc76bdb3b85a9443f7
compitcomds <compitcomds@gmail.com>
2024-04-08 15:42:44 +0530

    fgh

--- Solar/Admin/form.py
+++ Solar/Admin/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField,StringField,TextAreaField,FloatField,BooleanField,FloatField
+from wtforms import FileField, SubmitField,StringField,TextAreaField,FloatField,BooleanField,FloatField,IntegerField
 from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed 
 from flask_wtf.file import FileField, FileRequired
@@ -7,7 +7,7 @@ from flask_wtf.file import FileField, FileRequired
 
 
 class UplaodMainCategoriesForm(FlaskForm):
-    name = StringField('Enter the Name',validators=[DataRequired()])
+    name = StringField('Enter the Name',validators=[DataRequired(message='Name is required')])
     submit = SubmitField('Uplaod Main Categories Form')
 
 class UplaodSubCategoriesForm(FlaskForm):
@@ -27,17 +27,17 @@ class DynamicFormProduct:
     def create_form(field_names,data):
         if data != None:
             class DynamicForm(FlaskForm):
-                name = StringField('Name of product :', validators=[DataRequired()],default=data['name'])                
-                price = FloatField('Price of product :', validators=[DataRequired()],default=data['price'])
-                
-                brand= StringField('Brand :' ,validators=[DataRequired()],default=data['brand']) 
-                productid= StringField('Enter the ID :' ,validators=[DataRequired()],default=data['productid']) 
-                description = TextAreaField('Description of product :', validators=[DataRequired()],default=data['discription'],render_kw={"rows": 15, "cols": 50})
-                height = FloatField('Height of product :',validators=[DataRequired()],default=data['height'])
-                width = FloatField('Width of product :',validators=[DataRequired()],default=data['width'])
-                weight = FloatField('Weight of product :',validators=[DataRequired()],default=data['weight'])
-                minimumOrderUnit= FloatField('Minimum order quantity :', validators=[DataRequired()],default=data['minimumOrderUnit'])
-                top = BooleanField('Top',default=data['top'])
+                name = StringField('Name of product:', validators=[DataRequired(message="Please enter the name of the product.")], default=data['name'])                
+                price = FloatField('Price of product:', validators=[DataRequired(message="Please enter the price of the product."), NumberRange(min=0, message="Price must be a positive number.")], default=data['price'])
+                brand = StringField('Brand:', validators=[DataRequired(message="Please enter the brand of the product.")], default=data['brand']) 
+                productid = StringField('Enter the ID:', validators=[DataRequired(message="Please enter the ID of the product.")], default=data['productid']) 
+                description = TextAreaField('Description of product:', validators=[DataRequired(message="Please enter the description of the product.")], default=data['discription'], render_kw={"rows": 15, "cols": 50})
+                height = FloatField('Height of product:', validators=[DataRequired(message="Please enter the height of the product.Height must be a positive number."), NumberRange(min=0, message="Height must be a positive number.")], default=data['height'])
+                length = FloatField('Length of product:', validators=[DataRequired(message="Please enter the length of the product.Length must be a positive number."), NumberRange(min=0, message="Length must be a positive number.")], default=data['length'])
+                breadth = FloatField('Breadth of product:', validators=[DataRequired(message="Please enter the breadth of the product.Breadth must be a positive number."), NumberRange(min=0, message="Breadth must be a positive number.")], default=data['breadth'])
+                weight = FloatField('Weight of product:', validators=[DataRequired(message="Please enter the weight of the product. Weight must be a positive number."), NumberRange(min=0, message="Weight must be a positive number.")], default=data['weight'])
+                minimumOrderUnit = FloatField('Minimum order quantity:', validators=[DataRequired(message="Please enter the minimum order quantity."), NumberRange(min=0, message="Minimum order quantity must be a positive number.")], default=data['minimumOrderUnit'])
+                top = BooleanField('Top', default=data['top'])
             for field_name in field_names:
                 field_label = field_name['name']
                 try:
@@ -48,20 +48,21 @@ class DynamicFormProduct:
             return DynamicForm()
         else:
             class DynamicForm(FlaskForm):
-                name = StringField('Name of product :')
-                brand= StringField('Brand :' ,validators=[DataRequired()]) 
-                price = FloatField('Price of product :', validators=[DataRequired()])
-                productid= StringField('Enter the ID :' ,validators=[DataRequired()]) 
-                minimumOrderUnit= FloatField('Minimum order quantity :', validators=[DataRequired()],render_kw={"rows": 15, "cols": 50})
-                description = TextAreaField('Description of product :')
-                height = FloatField('Height of product :',validators=[DataRequired()])
-                width = FloatField('Width of product :',validators=[DataRequired()])
-                weight = FloatField('Weight of product :',validators=[DataRequired()])
+                name = StringField('Name of product:', validators=[DataRequired(message="Name is required.")])
+                brand = StringField('Brand:', validators=[DataRequired(message="Brand is required.")])
+                price = FloatField('Price of product:', validators=[DataRequired(message="Price is required.")])
+                productid = StringField('Enter the ID:', validators=[DataRequired(message="Product ID is required.")])
+                minimumOrderUnit = FloatField('Minimum order quantity:', validators=[DataRequired(message="Minimum order quantity is required.")], render_kw={"rows": 15, "cols": 50})
+                description = TextAreaField('Description of product:')
+                height = FloatField('Height of product:', validators=[DataRequired(message="Height is required. Height must be a positive number.")])
+                length = FloatField('Length of product:', validators=[DataRequired(message="Length is required. Length must be a positive number.")])
+                breadth = FloatField('Breadth of product:', validators=[DataRequired(message="Breadth is required. Breadth must be a positive number.")])
+                weight = FloatField('Weight of product:', validators=[DataRequired(message="Weight is required. Weight must be a positive number.")])
                 top = BooleanField('Top')
-                photos1 = FileField('Photos (635px X 563px)', validators=[DataRequired(),FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
-                photos2 = FileField('Photos (635px X 563px)', validators=[DataRequired(),FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
-                photos3 = FileField('Photos (635px X 563px)', validators=[DataRequired(),FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
-                photos4 = FileField('Photos (635px X 563px)', validators=[DataRequired(),FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
+                photos1 = FileField('Photos 1', validators=[DataRequired(message="Photo 1 is required."), FileAllowed(['jpg', 'jpeg', 'png'], message="Only JPG, JPEG, or PNG files allowed.")])
+                photos2 = FileField('Photos 2', validators=[DataRequired(message="Photo 2 is required."), FileAllowed(['jpg', 'jpeg', 'png'], message="Only JPG, JPEG, or PNG files allowed.")])
+                photos3 = FileField('Photos 3', validators=[DataRequired(message="Photo 3 is required."), FileAllowed(['jpg', 'jpeg', 'png'], message="Only JPG, JPEG, or PNG files allowed.")])
+                photos4 = FileField('Photos 4', validators=[DataRequired(message="Photo 4 is required."), FileAllowed(['jpg', 'jpeg', 'png'], message="Only JPG, JPEG, or PNG files allowed.")])
             for field_name in field_names:
                 setattr(DynamicForm, field_name['name'], StringField( field_name['name']))
             return DynamicForm()
